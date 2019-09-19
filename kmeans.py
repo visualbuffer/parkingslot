cd03e3dc7cb3baaf34ec09b30bd25d597dfa7a8f
Shuvashish Chatterjee <47427911+visualbuffer@users.noreply.github.com>
2019-09-20 02:19:18 +0530

    Update kmeans.py

--- kmeans.py
+++ kmeans.py
@@ -5,7 +5,7 @@ class YOLO_Kmeans:
 
     def __init__(self, cluster_number, filename):
         self.cluster_number = cluster_number
-        self.filename = "2012_train.txt"
+        self.filename = filename
 
     def iou(self, boxes, clusters):  # 1 box -> k clusters
         n = boxes.shape[0]
